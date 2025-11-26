e2972a7badf91d01eef9f6bd17cab9fd33a8e367
YotamHibner <tototheking123456789@gmail.com>
2025-11-26 15:01:49 +0200

    Add histogram building functionality
    Implemented histogram building logic in build_histogram function.

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,7 +1,12 @@
 # Q: Write an import statement that imports the function pyplot from the module matplotlib and renames it to plt.
-
 def build_histogram(data):
-    pass # Replace the `pass` with your code
+    histogram = {}
+    for i in data:
+      if i in histogram:
+        histogram[i] += 1
+      else:
+        histogram[i] = 1
+    return histogram
 
 def plot_histogram(histogram):
     x_values = list(histogram.keys())
